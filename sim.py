5d5f7c0305b61a48472f15090510dc6bd50cc91d
root <root@AY140521141233802d84Z.(none)>
2014-11-06 16:53:21 +0800

    use order instead of sort

--- sim.py
+++ sim.py
@@ -8,15 +8,14 @@ import sys
 name = sys.argv[1]
 df = pd.read_csv(name, index_col=0,  header=None)
 name = df.index
-df = scale(df)
+#df = scale(df)
 dist_out = pairwise_distances(df, metric="cosine")
 s = pd.DataFrame(dist_out, index=name, columns=name)
 for i in name:
     rs = ''
     rs += (str(i)+':')
     sim_rs = s.ix[i]
-    print type(sim_rs)
-    sim_rs = sim_rs.sort(inplace=False)
+    sim_rs = sim_rs.order()
     neighour = sim_rs[-20:]
     for k in neighour.index:
         rs += (str(k) + ',')
