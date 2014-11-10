0662dbfe6251f66f41050db10b66d0290b57a078
haoyuan.huhy <a7758526@gmail.com>
2014-11-10 11:20:21 +0800

    check the api and change sort to order, debug print is different

--- sim.py
+++ sim.py
@@ -4,20 +4,19 @@ from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import scale
 import sys
 
-
 name = sys.argv[1]
 num = int(sys.argv[2])
-df = pd.read_csv(name, index_col=0,  header=None)
-name = df.index
-df = scale(df)
+df = pd.read_csv(name,sep=',',  index_col=0,  header=None)
 dist_out = pairwise_distances(df, metric="cosine")
+name = df.index
 s = pd.DataFrame(dist_out, index=name, columns=name)
+
 for i in name:
     rs = ''
     rs += (str(i)+':')
     sim_rs = s.ix[i]
-    sim_rs = sim_rs.order()
+    sim_rs = sim_rs.order(ascending=False)
     neighour = sim_rs[:num]
-    for k in neighour.index:
+    for k in neighour.keys():
         rs += (str(k) + ',')
     print rs
