f0c64d63d157ade6f8adfd602944048947349de2
haoyuan.huhy <a7758526@gmail.com>
2014-11-10 11:44:42 +0800

    add id to print

--- daily_ratio.sh
+++ daily_ratio.sh
@@ -1,1 +1,1 @@
-cat output/name.csv | ./cmd.py './delta.py @1' |awk -F',' '{print "rat_"$1":"$2","$3}' |./writekv.py
+cat output/name.csv | ./cmd.py './delta.py @1' |awk -F',' '{print "rat_"$1":"$1","$2","$3}' |./writekv.py
